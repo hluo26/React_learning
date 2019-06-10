3db9c890f0e610745a853fdef8dae87b03054187
Hao Luo <luo1993826@gmail.com>
2019-06-09 20:28:21 -0700

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchInput from './SearchInput';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        response: []
+      };
+  }
+
+  handleSomething = (value) => {
+      console.log(value);
+        this.setState( {
+            response: value.abilities
+        })
+    }
+
+  render() {
+    return (
+        <div className="App">
+          <SearchInput handleSomething={this.handleSomething}/>
+            {
+                this.state.response !== null?
+                    <div>
+                        <ul>
+                            {this.state.response.map((x,index)=>
+                                <li key={index}>
+                                    {x.ability.name}
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                    :<h1>empty</h1>
+            }
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/SearchInput.js
@@ -0,0 +1,65 @@
+import React from "react";
+import SearchIcon from '@material-ui/icons/Search';
+
+class SearchInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            value: ''
+        };
+        this.getInfo = this.getInfo.bind(this);
+    }
+
+    getInfo (name){
+        fetch('https://pokeapi.co/api/v2/pokemon/'+name).then(
+            res => res.json()
+        ).then(
+            (result) => {
+                this.props.handleSomething(result)
+                this.setState({
+                        isLoaded: true,
+                    }
+                )
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: false,
+                    error
+                })
+            }
+        );
+    }
+
+    handleInputChange = event => {
+        const query = event.target.value;
+        this.setState({value:query});
+    };
+
+    handleKeyPress = event => {
+        if(event.charCode === 13 ) {
+            console.log('submitting ' + this.state.value);
+            this.getInfo(this.state.value);
+        }
+    };
+
+    render() {
+
+        return(
+            <div >
+                <div>
+                    <SearchIcon />
+                    <input
+                        value = {this.state.value}
+                        onChange = {this.handleInputChange}
+                        onKeyPress={this.handleKeyPress}
+                    />
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default SearchInput;
